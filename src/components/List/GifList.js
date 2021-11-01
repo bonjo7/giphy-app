a7a951810c3271ce762d911fc32923794e65e98d
Bernard Thompson <bethomps@redhat.com>
2021-11-01 10:55:06 +0000

    feat(embed): added modal to embed gifs via iframe and div for responsiveness

--- src/components/List/GifList.js
+++ src/components/List/GifList.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Container, Card, Row, Col } from "react-bootstrap";
-
+import React, { useState } from "react";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import GifModal from "../Modal/GifModal";
+import { Share } from "phosphor-react";
 import styles from "./Giflist.module.css";
 import Spinner from "../Spinner/Spinner";
 
 const GifList = ({ gifs, loading }) => {
+
+  const [show, setShow] = useState(false);
+  const [gif, setGif] = useState();
+  const handleClose = () => setShow(false);
+
+  const handleShow = (gifEmbed_URL) => {
+    setShow(true);
+    setGif(gifEmbed_URL)
+  };
   return (
     <>
+
+  <GifModal handleClose={handleClose} show={show} gif={gif} />
+
       {loading ? (
         <Spinner />
       ) : (
@@ -25,6 +38,7 @@ const GifList = ({ gifs, loading }) => {
                       <Card.Text className={styles.cardText}>
                         {gif.title.split("by")[0]}
                       </Card.Text>
+                      <Button onClick={() => handleShow(gif.embed_url)}><Share size={20} /></Button>
                     </Card.Body>
                   </Card>
                 </Col>
---
+++ src/components/Modal/GifModal.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Modal, Button, InputGroup, FormControl, Form } from "react-bootstrap";
+import styles from "./GifModal.module.css";
+
+const GifModal = ({ show, handleClose, gif }) => {
+  const [copy, setCopy] = useState(false);
+  const [responsive, setResponsive] = useState(false);
+
+  const IFrame = `<iframe src="${gif}"width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="${gif}">via GIPHY</a></p>`;
+  const responsiveIFrame = `<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="${gif}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="${gif}">via GIPHY</a></p>`
+
+  const onClickCopy = () => {
+    navigator.clipboard.writeText(IFrame);
+    setCopy(true);
+    setTimeout(() => {
+        setCopy(false);
+    }, 3000);
+   
+  };
+
+  const onSwitchAction = () => {
+    setResponsive(!responsive);
+  };
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        aria-labelledby='contained-modal-title-vcenter'
+        centered
+      >
+        <Modal.Header closeButton className={styles.modal}>
+          <Modal.Title style={{ color: "#1d44b8" }}>Embed GIF</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.modalBody}>
+          <p>
+            Want to embed this GIF on your website or blog? Just drop in the
+            embed code below and you're done!
+          </p>
+
+          <Form.Switch
+            onChange={onSwitchAction}
+            id='responsive-switch'
+            label='Responsive'
+            checked={responsive}
+            style={{marginBottom: "20px"}}
+          />
+
+          <InputGroup className='mb-3'>
+            <FormControl
+              aria-label='Iframe link'
+              aria-describedby='basic-addon2'
+              value={responsive ? responsiveIFrame : IFrame}
+             
+            />
+
+            <Button
+              variant='flat'
+              onClick={() => onClickCopy()}
+              id='button-addon2'
+              className={copy ? styles.buttonCopied : styles.buttonCopy}
+            >
+              {copy ? "Copied" : "Copy Code"}
+            </Button>
+          </InputGroup>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default GifModal;
